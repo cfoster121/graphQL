97357a01d124b66ae2cbda5ffb480ab97081cd32
cfoster121 <cfoster649@gmail.com>
2023-04-02 12:04:00 -0700

    Set up job detail query for individual jobs/descriptions/paths

--- projects/job-board/server/resolvers.js
+++ projects/job-board/server/resolvers.js
@@ -2,6 +2,7 @@ import { Company, Job } from './db.js'
 
 export const resolvers = {
     Query: {
+        job: async (root, {id}) => Job.findById(id),
         jobs: async () => Job.findAll()
     },
 
