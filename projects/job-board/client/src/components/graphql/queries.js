af46d743418c66a44ca3d791c3ee8cb693f6a86b
cfoster121 <cfoster649@gmail.com>
2023-04-04 16:20:59 -0700

    Complete section 6 (apollo client)

--- projects/job-board/client/src/components/graphql/queries.js
+++ projects/job-board/client/src/components/graphql/queries.js
@@ -1,7 +1,24 @@
-import { request, gql } from 'graphql-request';
+import { gql, ApolloClient, InMemoryCache } from '@apollo/client';
+import { request } from 'graphql-request';
 import { getAccessToken } from '../../auth';
 
 const GRAPHQL_URL = 'http://localhost:9000/graphql'
+
+const client = new ApolloClient({
+  uri: GRAPHQL_URL,
+  cache: new InMemoryCache(),
+})
+
+const JOB_DETAIL_FRAGMENT = gql`
+fragment JobDetail on Job {
+    id
+    title
+    company {
+      id
+      name
+    }
+    description
+}`
 
 export async function getCompany(id) {
     const query = gql`
@@ -17,7 +34,7 @@ export async function getCompany(id) {
   }
    }`;
    const variables = { id };
-   const { company } = await request (GRAPHQL_URL, query, variables);
+   const {data: {company}} = await client.query({ query, variables });
    return company;
 }
 
@@ -25,24 +42,19 @@ export async function getJob(id) {
     const query = gql`
     query JobQuery ($id: ID!) {
   job(id: $id) {
-    id
-    title
-    company {
-      id
-      name
-    }
-    description
+   ...JobDetail
   }
-}`;
+}
+${JOB_DETAIL_FRAGMENT}`;
     const variables = { id };
-    const { job } = await request(GRAPHQL_URL, query, variables)
+    const {data: {job}} = await client.query({ query, variables });
     return job;
 
 }
 
 export async function getJobs() {
     const query = gql`
-    query {
+    query JobsQuery {
   jobs {
     id
     title
@@ -54,20 +66,26 @@ export async function getJobs() {
   }
 }
     `;
-    const { jobs } = await request(GRAPHQL_URL, query)
+    const {data: {jobs}} = await client.query({ 
+      query,
+      fetchPolicy: "network-only"
+    })
     return jobs;
 }
 
 export async function createJob(input) {
-  const query = gql`
+  const mutation = gql`
   mutation createJobMutation($input: CreateJobInput!) {
   job: createJob (input: $input) {
-    id
+    ...JobDetail
   }
-}`;
+}
+${JOB_DETAIL_FRAGMENT}`;
   const variables = {input};
-  const headers = {'Authorization': "Bearer " + getAccessToken()};
-  const {job} = await request(GRAPHQL_URL, query, variables, headers)
+  const context = {
+    headers : {'Authorization': "Bearer " + getAccessToken()}
+  }
+  const { data: { job }} = await client.mutate({ mutation, variables, context });
   return job
 }
 
